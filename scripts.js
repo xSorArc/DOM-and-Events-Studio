398e8af5d21f13960d6cfdcae887ee16e58e9ce0
xSorArc <87716789+xSorArc@users.noreply.github.com>
2022-07-11 08:41:46 -0500

    Added flightStatus change for takeoff.

--- scripts.js
+++ scripts.js
@@ -1,2 +1,15 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function init() {
+    const status = document.getElementById("flightStatus");
+
+    let takeoff = document.getElementById("takeoff");
+    takeoff.addEventListener("click", event => {
+        window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if (true) {
+            status.innerHTML = "Shuttle in flight."
+        }
+    })
+}
+
+window.addEventListener("load", init)
