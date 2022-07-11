6633d9e2fc0bf03cf6474dc65bb20375e8124119
xSorArc <87716789+xSorArc@users.noreply.github.com>
2022-07-11 09:30:30 -0500

    Added landing features to land button.

--- scripts.js
+++ scripts.js
@@ -7,12 +7,20 @@ function init() {
 
     let takeoff = document.getElementById("takeoff");
     takeoff.addEventListener("click", event => {
-        window.confirm("Confirm that the shuttle is ready for takeoff.");   //a
+        window.confirm("Confirm that the shuttle is ready for takeoff.");   //2a
         if (true) {
-            status.innerHTML = "Shuttle in flight."                         //b
-            shuttleBackground.style.background = "blue";                    //c
-            height.innerHTML = 10000;                                       //d
+            status.innerHTML = "Shuttle in flight."                         //2b
+            shuttleBackground.style.background = "blue";                    //2c
+            height.innerHTML = 10000;                                       //2d
         }
+    })
+    
+    let land = document.getElementById("landing");
+    land.addEventListener("click", event => {
+        window.alert("The shuttle is landing. Landing gear engaged.");      //3a
+        status.innerHTML = "The shuttle has landed.";                       //3b
+        shuttleBackground.style.background = "";                            //3c
+        height.innerHTML = 0;                                               //3d
     })
 }
 
