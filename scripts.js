29cf0d43973f0564ec5448e8217d8636e33fa53f
xSorArc <87716789+xSorArc@users.noreply.github.com>
2022-07-11 09:36:11 -0500

    Added features to abort mission button.

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ function init() {
     const shuttleBackground = document.getElementById("shuttleBackground");
     const height = document.getElementById("spaceShuttleHeight");
 
-    let takeoff = document.getElementById("takeoff");
+    const takeoff = document.getElementById("takeoff");
     takeoff.addEventListener("click", event => {
         window.confirm("Confirm that the shuttle is ready for takeoff.");   //2a
         if (true) {
@@ -14,13 +14,23 @@ function init() {
             height.innerHTML = 10000;                                       //2d
         }
     })
-    
-    let land = document.getElementById("landing");
+
+    const land = document.getElementById("landing");
     land.addEventListener("click", event => {
         window.alert("The shuttle is landing. Landing gear engaged.");      //3a
         status.innerHTML = "The shuttle has landed.";                       //3b
         shuttleBackground.style.background = "";                            //3c
         height.innerHTML = 0;                                               //3d
+    })
+
+    const abort = document.getElementById("missionAbort");
+    abort.addEventListener("click", event => {
+        window.confirm("Confirm that you want to abort the mission.");      //4a
+        if (true) {
+            status.innerHTML = "Mission aborted.";                          //4b
+            shuttleBackground.style.background = "";                        //4c
+            height.innerHTML = 0;                                           //4d
+        }
     })
 }
 
