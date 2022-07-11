de41b5947c6bba084363ab8706c063cc91498ee9
xSorArc <87716789+xSorArc@users.noreply.github.com>
2022-07-11 08:49:43 -0500

    Added height change to takeoff

--- scripts.js
+++ scripts.js
@@ -2,12 +2,16 @@
 // Remember to pay attention to page loading!
 function init() {
     const status = document.getElementById("flightStatus");
+    const shuttleBackground = document.getElementById("shuttleBackground");
+    const height = document.getElementById("spaceShuttleHeight");
 
     let takeoff = document.getElementById("takeoff");
     takeoff.addEventListener("click", event => {
-        window.confirm("Confirm that the shuttle is ready for takeoff.");
+        window.confirm("Confirm that the shuttle is ready for takeoff.");   //a
         if (true) {
-            status.innerHTML = "Shuttle in flight."
+            status.innerHTML = "Shuttle in flight."                         //b
+            shuttleBackground.style.background = "blue";                    //c
+            height.innerHTML = 10000;                                       //d
         }
     })
 }
